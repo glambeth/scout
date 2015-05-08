7e321fa3b1527df4020a1a7ba5a742683dfa855e
glambeth <glambeth1594@gmail.com>
2015-05-07 23:16:54 -0700

    Mongooose

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ var config = require('./config');
 var mongoose = require("mongoose");
 
 var app = express();
+
+
+mongoose.connect(config.database, function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Connected to the database');
+	}
+});
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
